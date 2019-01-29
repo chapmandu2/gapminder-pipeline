79798e45f962855610d09946f8e9b0ec7369591c
Phil Chapman <chapmandu2@gmail.com>
2019-01-29 16:03:08 +0000

    03: neaten up the deployment mechanism

--- 03-plumber-api/plumber-apis/deploy.R
+++ 03-plumber-api/plumber-apis/deploy.R
@@ -1,7 +1,16 @@
 # deploy api script
 
-api_script <- file.path('/plumber/plumber-apis', Sys.getenv('API_SCRIPT'))
+if(nchar(Sys.getenv('API_SCRIPT')) > 0) {
+  # deploy
+  api_script <- file.path('/plumber/plumber-apis', Sys.getenv('API_SCRIPT'))
+  port_no <- 8001
+} else {
+  # dev
+  api_script <- '03-plumber-api/plumber-apis/gapminder-api.R'
+  port_no <- 8000
+}
+
 stopifnot(file.exists(api_script))
 
 pr <- plumber::plumb(api_script)
-pr$run(host='0.0.0.0', port=8000)
+pr$run(host='0.0.0.0', port=port_no) #view at localhost:8000 or localhost:8001 depending on run or deploy
